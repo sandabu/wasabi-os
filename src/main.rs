b719af32fd1bf3cdd25e545ac3d9712ecc2b3be2
sandabu <nao0101.n@gmail.com>
2026-04-25 00:26:39 +0900

    p478

--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ use wasabi::qemu::exit_qemu;
 use wasabi::qemu::QemuExitCode;
 use wasabi::uefi::init_vram;
 use wasabi::uefi::EfiHandle;
+use wasabi::uefi::locate_loaded_image_protocol;
 use wasabi::uefi::EfiMemoryType;
 use wasabi::uefi::EfiSystemTable;
 use wasabi::uefi::VramTextWriter;
@@ -31,6 +32,11 @@ fn efi_main(image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
     println!("Booting WasabiOS...\n");
     println!("image_handle:{:#018X}\n", image_handle);
     println!("efi_system_table:{:#p}\n", efi_system_table);
+    let loaded_image_protocol = 
+        locate_loaded_image_protocol(image_handle, efi_system_table)
+        .expect("Failed to get LoadedImageProtocol");
+    println!("image_base {:018X}", loaded_image_protocol.image_base);
+    println!("image_size {:018X}", loaded_image_protocol.image_size);
     info!("info!");
     warn!("warn!");
     error!("error!");
