93ac833174ffdf36baba1406a88ce0ede334f5e6
sandabu <nao0101.n@gmail.com>
2026-05-23 00:31:25 +0900

    p688

--- src/init.rs
+++ src/init.rs
@@ -6,6 +6,7 @@ use crate::graphics::draw_test_pattern;
 use crate::graphics::fill_rect;
 use crate::graphics::Bitmap;
 use crate::info;
+use crate::pci::Pci;
 use crate::uefi::exit_from_efi_boot_services;
 use crate::uefi::EfiHandle;
 use crate::uefi::EfiMemoryType;
@@ -95,5 +96,7 @@ pub fn init_pci(acpi: &AcpiRsdpStruct) {
                 info!("{}", e)
             }
         }
+        let pci = Pci::new(mcfg);
+        pci.probe_devices();
     }
 }
